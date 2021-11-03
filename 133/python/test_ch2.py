e4384046c65c14f67885269fdec7cd3451bfb5f7
Evan Cope <copev313@gmail.com>
2021-11-03 11:47:49 -0400

    challenge133: added task2 test placeholders

---
+++ 133/python/test_ch2.py
@@ -0,0 +1,18 @@
+import pytest
+from ch2 import prime_factors, smith_number, generate_smith_numbers
+
+
+class TestPrimeFactorization:
+    def test_always_passes(self):
+        assert True
+
+
+class TestSmithNumber:
+    def test_always_passes(self):
+        assert True
+
+
+class TestGenerateSmithNumbers:
+    def test_always_passes(self):
+        assert True
+
